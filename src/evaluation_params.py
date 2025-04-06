a15641023e3e00a0a57bc4e361a659acc3c797a3
yavuzff <75276351+yavuzff@users.noreply.github.com>
2025-04-06 22:46:46 +0100

    add plotting of various large-scale experiments

--- src/evaluation_params.py
+++ src/evaluation_params.py
@@ -1,18 +1,22 @@
 import numpy as np
 
+
 class Params:
-    def __init__(self,modalities: int, dimensions: list[int], metrics: list[str], weights: list[float], dataset: list[np.ndarray], index_size: int, k: int, query_ids: list[int]):
+    def __init__(self, modalities: int, dimensions: list[int], metrics: list[str], weights: list[float],
+                 dataset: list[np.ndarray] = None, index_size: int = None, k: int = None, query_ids: list[int] = None):
         self.modalities = modalities
         self.dimensions = dimensions
         self.metrics = metrics
         self.weights = weights
         self.dataset = dataset
         self.index_size = index_size
-        if self.index_size > len(dataset[0]):
-            print(f"Warning: requested index size {self.index_size} < dataset size ({len(dataset[0])}. Setting index_size to {len(dataset[0])}")
+        if dataset is not None and self.index_size > len(dataset[0]):
+            print(
+                f"Warning: requested index size {self.index_size} < dataset size ({len(dataset[0])}. Setting index_size to {len(dataset[0])}")
             self.index_size = len(dataset[0])
         self.k = k
         self.query_ids = query_ids
+
 
 class MultiVecHNSWConstructionParams:
     def __init__(self, target_degree: int, max_degree: int, ef_construction: int, seed: int):
@@ -22,8 +26,8 @@ class MultiVecHNSWConstructionParams:
         self.ef_construction = ef_construction
         self.seed = seed
 
+
 class MultiVecHNSWSearchParams:
-    def __init__(self, k:int, ef_search: int):
+    def __init__(self, k: int, ef_search: int):
         self.k = k
         self.ef_search = ef_search
-
